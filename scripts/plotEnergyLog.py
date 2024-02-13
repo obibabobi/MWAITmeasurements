96e2034d5dbd24b0794f5db74b41925b9241847a
Thomas Oberhauser <thomas-oberhauser@t-online.de>
2024-02-13 18:06:50 +0100

    Added script for visualizing power_log.csv

---
+++ scripts/plotEnergyLog.py
@@ -0,0 +1,16 @@
+import pandas
+import matplotlib.pyplot as plt
+import os
+
+scriptDir = os.path.dirname(__file__)
+outputDir = os.path.normpath(os.path.join(scriptDir, '..', 'output'))
+powerLogFile = os.path.join(outputDir, 'power_log.csv')
+
+def plotPowerLog():
+	df = pandas.read_csv(powerLogFile)
+	
+	plot = df.plot('time', 'power')
+	plot.set_ylim(ymin=0)
+	plt.show()
+
+plotPowerLog()
