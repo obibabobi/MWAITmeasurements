e6a51a650b77f7bf59892e2eaea26adee0fc1358
Thomas Oberhauser <thomas-oberhauser@t-online.de>
2024-02-19 17:50:24 +0100

    Ensured correct directory

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-echo "rm -rf /root/mwait_deploy/*" | ssh root@$1 'bash -s'
+# preparation
+pushd "$(dirname "$0")"
+
+echo "rm -rf /root/mwait_deploy/*" | ssh root@$1 'bash -s' &&
 rsync -r mwait_deploy root@$1:/root/ &&
 echo "cd mwait_deploy && make" | ssh root@$1 'bash -s'
+
+popd
--- measure.sh
+++ measure.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# preparation
+pushd "$(dirname "$0")"
+
 (echo "mwait_deploy/measure.sh $2" | ssh root@$1 'bash -s') &&
 rm -rf output/* &&
-rsync -r root@$1:/root/mwait_deploy/results/ output/results/ &&
-python3 scripts/evaluateMeasurements.py output/results/
+rsync -r root@$1:/root/mwait_deploy/results/ output/results/
+
+popd
