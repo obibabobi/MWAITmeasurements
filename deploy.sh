b2cfbd020f0cf49e58728993403b72dc11bed8cf
Thomas Oberhauser <thomas-oberhauser@t-online.de>
2024-02-20 15:55:38 +0100

    Added help function

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,35 @@
 #!/bin/bash
 
+help() {
+    echo "######################"
+    echo "### Deploy script ###"
+    echo "######################"
+    echo
+    echo "Syntax: deploy.sh <ip> [-h]"
+    echo "Description:"
+    echo "    <ip>: The IP address of the measurebox"
+    echo
+    echo "    -h: Print help, then quit"
+}
+
+while getopts ":h" option; do
+    case $option in
+        h)
+            help
+            exit;;
+   esac
+done
+
 # preparation
 pushd "$(dirname "$0")"
 
-echo "rm -rf /root/mwait_deploy/*" | ssh root@$1 'bash -s' &&
-rsync -r mwait_deploy root@$1:/root/ &&
+# remove any files from previous deployments
+echo "rm -rf /root/mwait_deploy/*" | ssh root@$1 'bash -s'
+
+# copy the necessary sources
+rsync -r mwait_deploy root@$1:/root/
+
+# compile
 echo "cd mwait_deploy && make" | ssh root@$1 'bash -s'
 
 popd
